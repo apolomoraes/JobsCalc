1e0a6f552212c3f8019f16e53e629d61dd93cde7
Apolo Moraes <vlogsapolo@gmail.com>
2023-04-29 19:04:31 -0300

    add informations of profile.ejs

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,15 @@ const routes = express.Router();
 
 const views = __dirname + "/views/";
 
+const profile = {
+  name: "Apolo Moraes",
+  avatar: "https://avatars.githubusercontent.com/u/85043318?v=4",
+  "monthly-budget": 3000,
+  "days-per-week": 6,
+  "hours-per-day": 6,
+  "vacation-per-year": 4
+}
+
 routes.get("/", (req, res) => {
   return res.render(views + "index");
 });
@@ -15,7 +24,7 @@ routes.get("/job/edit", (req, res) => {
   return res.render(views + "job-edit");
 });
 routes.get("/profile", (req, res) => {
-  return res.render(views + "profile");
+  return res.render(views + "profile", { profile });
 });
 
 module.exports = routes;
